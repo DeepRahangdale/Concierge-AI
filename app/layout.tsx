a0936c76d5a99459a10c7117c57e7e4abee568b4
DeepRahangdale <deeprahangdale2003@gmail.com>
2023-10-13 02:31:56 +0530

    Icons Change

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,14 +7,14 @@ import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from '@/components/ui/toaster';
 import { ProModal } from '@/components/pro-modal';
 import { Analytics } from '@vercel/analytics/react';
-
+import Head from 'next/head';
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Concierge.AI',
-  description: 'Your customized companion.',
+  title: 'Concierge AI',
+  description: 'Your Customized Companion.',
 }
 
 export default function RootLayout({
@@ -24,6 +24,10 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
+       <Head>
+        <link rel="icon" href="./robot.png" /> {/* Make sure to use the correct path to your favicon */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <html lang="en" suppressHydrationWarning>
         <body className={cn("bg-secondary", inter.className)}>
           <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
